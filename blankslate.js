91787bc3c35afb834a66ce64fe425613115f7093
Jon Tucker <jazzviking@gmail.com>
2023-12-26 14:48:52 -0500

    first commit

---
+++ blankslate.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const scoreBubbles = document.querySelectorAll('.score_bubble');
+
+const toggleScoreBubble = (e) => {
+  let activeLocationIndex;
+  e.target.classList.toggle('active');
+
+  const activeLocation = document.querySelectorAll('.score_bubble');
+  console.log(activeLocation);
+};
+
+scoreBubbles.forEach((bubble) =>
+  bubble.addEventListener('click', toggleScoreBubble)
+);
